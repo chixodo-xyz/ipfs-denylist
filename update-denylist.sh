15db302acc4a2e6cb833fef9bacc74a9c3aa7f0c
Josias Bruderer <josias@macherstube.ch>
2022-11-28 13:45:01 +0100

    fix path

---
+++ update-denylist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+folder="/usr/share/denylist"
+
+rm -rf $folder
+
+mkdir $folder
+for i in {0..255}; do mkdir $folder/$(printf "%02x" $i); done
+
+curl -sN https://badbits.dwebops.pub/denylist.json | jq -r '.[].anchor' | while read -r line; do echo "$line" >> $folder/${line:0:2}/${line:0:4}; done
+
+echo "denylist updated"
